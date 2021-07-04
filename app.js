7a5c3cce414a266a3e41c9602807d826743928d4
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-07-04 14:00:50 +0300

    added index to view page

---
+++ app.js
@@ -0,0 +1,8 @@
+const startInterview = () => {
+    let fullName = prompt("what's your full name?");
+    let age = prompt("enter you age please");
+    let major = prompt("what's your university major?");
+    let university = prompt("where did you study?");
+
+    alert(`Hello ${fullName}, you're still young with ${age} years old, also, ${major} is one of the best majors these days and ${university} is one of the best in the region`);
+}
