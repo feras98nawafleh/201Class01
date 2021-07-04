e1242b832397a3d509553bf4bb244ffe4b205cb4
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-07-04 15:05:17 +0300

    added index and followed the assignment instructions

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const startInterview = () => {
     let fullName = prompt("what's your full name?");
+    console.log(fullName);
+
     let age = prompt("enter you age please");
+    console.log(age);
+
     let major = prompt("what's your university major?");
+    console.log(major);
+
     let university = prompt("where did you study?");
+    console.log(university);
 
     alert(`Hello ${fullName} nice to meet you, you're still young with ${age} years old, also, ${major} is one of the best majors these days and ${university} is one of the best in the region`);
 }
